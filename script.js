68319c667e712bdb645dabe353a33efbbbf875c5
shivani-sahu1981 <shivanisahu1981@gmail.com>
2025-01-30 13:39:47 +0530

    gjg

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
+// const radius = [1,2,3,4];
+
+// const diameter = function(radius){
+//     const output = [];
+//     for( let i=0;  i<=radius.length; i++){
+//         output.push(2 + radius[i]);
+//     }
+//     return output;
+// }
+
+// const area = function(radius){
+//     const output = [];
+//     for( let i=0;  i<radius.length; i++){
+//         output.push(Math.PI * radius[i] * radius[i]);
+//     }
+//     return output;
+// }
+
+// console.log(diameter(radius));
+// console.log(area(radius));
+
 const radius = [1,2,3,4];
 
-const diameter = function(radius){
-    const output = [];
-    for( let i=0;  i<=radius.length; i++){
-        output.push(2 + radius[i]);
-    }
-    return output;
-}
-
 const area = function(radius){
+    return Math.PI = radius *radius;
+};
+
+const diameter = function(radius){
+    return 2 * radius;
+};
+
+const calculate = function(radius, logic){
     const output = [];
     for( let i=0;  i<radius.length; i++){
-        output.push(Math.PI * radius[i] * radius[i]);
+        output.push(logic(radius[i]));
     }
     return output;
 }
 
-console.log(diameter(radius));
-console.log(area(radius));
+console.log(calculate(radius, diameter));
+console.log(calculate(radius, area));
